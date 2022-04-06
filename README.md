8b28ae7441442447761f9142db2907732b51a303
jcorredorc <jcorredorc@github.com>
2022-04-06 16:05:18 -0500

    fix: pid version of turtlebot3 moving whitout hit obstacles

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ This repository is solely for storing notes on the udemy course's personal learn
 |[iot_sensor](src/iot_sensor/) | use a custom message [IoTSensor.msg](msg/IoTSensor.msg), publish and subscribe iot msg  |
 |[service_example](src/service_example/)|a service that calculate the area of a rectangle, given its width and height. See the definition of the service type in [srv file](srv/rectangleAreaService.srv)|
 |[opencv_example](src/opencv_example/)| **Python** <br/> * open_copy.py <br/> * image_structure.py <br/> * images_encoding.py <br/> * read_video.py <br/> * image_draw.py <br/> * image_pub_sub.py (bridge w ROS) <br/>  * image_thresholding.py (segmentation) <br/> * color_filtering.py <br/> * contours_detection.py <br/> * contours_processing.py <br/> * ball_detection.py <br/>  **C++**  <br/>  * read_video.cpp <br/>  * open_copy.cpp <br/> * image_pub_sub.cpp <br/>  *  trackbar_video.cpp (book learning OpenCV) <br/> **Assignments** <br/> * read_video_ball_tracker.cpp  <br/> * tennis_ball_publisher.cpp <br/> * tennis_ball_listener.cpp <br/> * tennis_ball_usb_cam_tracker.cpp  |
-|[scan_laser_example](src/scan_laser_example/)|* scan_subscriber.py <br/> * scan_subscriber.cpp <br/> * robot_stops_obstacle.py (:x:) <br/> * laserscan: A custom library from the [course material](https://www.udemy.com/course/ros-essentials/) <br/> **Assignment** <br/> * move_stop_turtlebot3.cpp <br/>  * move_pid_turtlebot3.cpp|
+|[scan_laser_example](src/scan_laser_example/)|* scan_subscriber.py <br/> * scan_subscriber.cpp <br/> * robot_stops_obstacle.py :x: <br/> * laserscan: A custom library from the [course material](https://www.udemy.com/course/ros-essentials/) <br/> **Assignment** <br/> * move_stop_turtlebot3.cpp <br/>  * move_pid_turtlebot3.cpp|
 
 
 ## Usage
@@ -38,7 +38,7 @@ sudo apt install xterm
     - Test the functions in other map
 
     ```
-    rosrun udemy_ros_beginners turtlebot3_start.sh
+    rosrun udemy_ros_beginners turtlebot3_start_house.sh
     ```
     -  Proportional controller that regulates the angular speed and the linear speed such as it moves smoothly without hitting obstacles.
 
@@ -46,5 +46,10 @@ sudo apt install xterm
     rosrun udemy_ros_beginners turtlebot3_pid_start.sh
     ```
 
+    - Test the proportional controller in other map
 
+    ```
+    rosrun udemy_ros_beginners turtlebot3_pid_start_house.sh
+    ```
 
+    >  Play with parameters: left_angle, right_angle and proportional gain (Prop). 
--- src/scan_laser_example/assignment/move_pid_turtlebot3.cpp
+++ src/scan_laser_example/assignment/move_pid_turtlebot3.cpp
@@ -2,6 +2,7 @@
 #include <sensor_msgs/LaserScan.h>
 #include <geometry_msgs/Twist.h>
 #include "../laserscan/LaserScanner.h"
+// #include "../laserscan/utility_lib.h"
 
 using namespace std;
 
@@ -10,11 +11,12 @@ ros::Subscriber scanSubscriber;
 ros::Publisher controlled_cmd_vel_publ;
 geometry_msgs::Twist cmd_vel_command;
 // bool Obstacle;
-
+float right_angle=5, left_angle=5, Prop=0.5;
 
 void scanCallback(sensor_msgs::LaserScan scanMessage);
 bool moveStraightUntilObstacle(sensor_msgs::LaserScan scanMessage);
 void rotateUntilNoObstacle(sensor_msgs::LaserScan scanMessage, bool Obstacle);
+void publish_vel_command(float speed, float angle);
 
 int main(int argc, char **argv){
 
@@ -38,20 +40,21 @@ void scanCallback (sensor_msgs::LaserScan scanMessage){
 }
 
 bool moveStraightUntilObstacle(sensor_msgs::LaserScan scanMessage){
-
-	if (LaserScanner::getMinimumFrontRange(_scanMsg,5,5) < 0.6){
-		cmd_vel_command.linear.x =0.0;
-		// cmd_vel_command.angular.z=0.0;
-		controlled_cmd_vel_publ.publish(cmd_vel_command);
-		ROS_INFO("Front distance minimum reached!! [%2.2f], linear Vel x [%2.2f] ", 
-										LaserScanner::getMinimumFrontRange(scanMessage,5,5), 
-										cmd_vel_command.linear.x);
+	// Prop = 0.5;
+	if (LaserScanner::getMinimumFrontRange(_scanMsg,left_angle,right_angle) < 0.6){
+		// cmd_vel_command.linear.x =0.0;
+		// // cmd_vel_command.angular.z=0.0;
+		// controlled_cmd_vel_publ.publish(cmd_vel_command);
+		publish_vel_command(0,0);
+		ROS_INFO("Front distance minimum reached!! [%2.2f]", 
+						LaserScanner::getMinimumFrontRange(_scanMsg,left_angle,right_angle));
 		return true;
 	} else{
-		cmd_vel_command.linear.x =-0.5*(0.6-LaserScanner::getMinimumFrontRange(_scanMsg,5,5));
-		cmd_vel_command.angular.z=0;
-		controlled_cmd_vel_publ.publish(cmd_vel_command);
-		ROS_INFO("linear vel x [%2.2f]",cmd_vel_command.linear.x);
+		// cmd_vel_command.linear.x =-0.5*(0.6-LaserScanner::getMinimumFrontRange(_scanMsg,5,5));
+		// cmd_vel_command.angular.z=0;
+		// controlled_cmd_vel_publ.publish(cmd_vel_command);
+		// ROS_INFO("linear vel x [%2.2f]",cmd_vel_command.linear.x);
+		publish_vel_command(-Prop*(0.6-LaserScanner::getMinimumFrontRange(_scanMsg,left_angle,right_angle)),0);
 		return false;
 
 	}
@@ -60,28 +63,32 @@ bool moveStraightUntilObstacle(sensor_msgs::LaserScan scanMessage){
 void rotateUntilNoObstacle(sensor_msgs::LaserScan scanMessage, bool Obstacle_true=true){
 	// ROS_INFO_STREAM("Inside rotate Obstacle: "<< Obstacle_true);
 	// double distance_front = LaserScanner::getMinimumFrontRange(scanMessage,5,5);
+	float angle_vel=0;
 	if (Obstacle_true){
 			// ros::Rate loop_rate(10);
 		while (ros::ok()){
 			ros::spinOnce();
-			cmd_vel_command.linear.x=-0.5*(0.6-LaserScanner::getMinimumFrontRange(_scanMsg,5,5));
 			// cmd_vel_command.angular.z=0.3;
-			if (LaserScanner::getMinimumRangeLeft(_scanMsg,5) < LaserScanner::getMinimumRangeRight(_scanMsg,5)){
-				cmd_vel_command.angular.z=0.3;
+			if (LaserScanner::getMinimumRangeLeft(_scanMsg,left_angle) < LaserScanner::getMinimumRangeRight(_scanMsg,right_angle)){
+				angle_vel = 0.3;
+				// cmd_vel_command.angular.z=0.3;
 				// ros::Duration(1).sleep();
 			}
 			else{
-				cmd_vel_command.angular.z=-0.3;
-				// ros::Duration(1).sleep();
+				angle_vel = -0.3;
+				// cmd_vel_command.angular.z=-0.3;
 			}
-			// ROS_INFO("Angular vel [%2.2f], distance: [%2.2f],",cmd_vel_command.angular.z, 
-															//    distance_front);
-			 ROS_INFO("Linear vel [%2.2f] | Angular vel [%2.2f]",cmd_vel_command.linear.x,
-			 													 cmd_vel_command.angular.z);
-			controlled_cmd_vel_publ.publish(cmd_vel_command);
+			// cmd_vel_command.linear.x=-0.5*(0.6-LaserScanner::getMinimumFrontRange(_scanMsg,left_angle,right_angle));
+			// controlled_cmd_vel_publ.publish(cmd_vel_command);
+			//  ROS_INFO("Linear vel [%2.2f] | Angular vel [%2.2f]",cmd_vel_command.linear.x,
+			//  													 cmd_vel_command.angular.z);
+			publish_vel_command(-Prop*(0.6-LaserScanner::getMinimumFrontRange(_scanMsg,
+													left_angle,right_angle)),
+													angle_vel);
+			ros::Duration(1).sleep();
 			// ros::Duration(1).sleep();
 			// ROS_INFO("_scanMsg: [%2.2f]",LaserScanner::getMinimumFrontRange(_scanMsg,5,5));
-			if (LaserScanner::getMinimumFrontRange(_scanMsg,5,5)> 3){
+			if (LaserScanner::getMinimumFrontRange(_scanMsg,left_angle,right_angle)> 3){
 				break;
 			}
 			// loop_rate.sleep(); 
@@ -91,7 +98,27 @@ void rotateUntilNoObstacle(sensor_msgs::LaserScan scanMessage, bool Obstacle_tru
 			// }
 		}
 	} else{
-		ROS_INFO_STREAM("no obstacle");
+		ROS_INFO_STREAM("no obstacle rotating");
 	}
 	// return true;
+}
+
+void publish_vel_command(float speed, float angle){
+	// Saturation at 1.0 ------------
+	float max_speed = 1.0;
+	int sign =0;
+	if (speed >=0){
+		sign=1;
+	}else{
+		sign=-1;
+	}
+	if (abs(speed) > max_speed){
+		speed = sign*max_speed;
+	}
+	//-------------------------------	
+	cmd_vel_command.linear.x = speed;
+	cmd_vel_command.angular.z= angle;
+	controlled_cmd_vel_publ.publish(cmd_vel_command);
+	ROS_INFO("Linear vel [%2.2f] | Angular vel [%2.2f]",cmd_vel_command.linear.x,
+			 													 cmd_vel_command.angular.z);
 }
